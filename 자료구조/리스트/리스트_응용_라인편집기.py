81467942ca550514762b5aac76dae7008237af7a
kouydong <kouydong@aegisep.com>
2023-03-13 17:58:32 +0900

    첫번째 커밋

--- 자료구조/리스트/리스트_응용_라인편집기.py
+++ 자료구조/리스트/리스트_응용_라인편집기.py
@@ -4,7 +4,9 @@ def myLineEditor():
     while True:
         command = input("[메뉴선택] i-입력, d-삭제, r-변경, p-출력, 1-파일읽기, s-저장, q-종료 =>")
 
-    if command == "i":                  # 삽입연산
-        listIndex = int(input("입력행 번호:"))  # 사입할 행번호 입력
-        str = input("입력행 내용.")
-        list.insert(listIndex)
+    if command == "i":                         # 삽입연산
+        list_index = int(input("입력행 번호:"))   # 사입할 행번호 입력
+        str = input("입력행 내용.")              #
+        list.insert(listIndex)                 # 리스트 데이터 추가
+    elif command == "d":
+        listIndex = ""
